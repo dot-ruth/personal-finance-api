122183fccdcb5229e8e4b2149979561a576ebd76
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-29 16:43:34 +0300

    FEAT|#5|added get by Id,Update and Delete functionalities

--- Controllers/IncomeController.cs
+++ Controllers/IncomeController.cs
@@ -27,4 +27,30 @@ public class IncomeController : ControllerBase
         return await dbContext.Income.ToListAsync();
     }
 
+     [HttpGet("{id}")]
+    public async Task<ActionResult<Income>> GetIncomeById(int id)
+    {
+        return await dbContext.Income.FindAsync(id);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateIncome(int id, Income income)
+    {
+        if (id != income.Id) return BadRequest();
+        dbContext.Entry(income).State = EntityState.Modified;
+        await dbContext.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteIncome(int id)
+    {
+        var income = await dbContext.Income.FindAsync(id);
+        dbContext.Income.Remove(income);
+        await dbContext.SaveChangesAsync();
+        return NoContent();
+    }
+
+
+
 }
